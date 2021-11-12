e70be88a33675a9f82ea2a8cdb8765d5f6c58326
sling254 <mwanikidenis7@gmail.com>
2021-11-13 00:06:56 +0300

    add profile view and authentication to the navbar

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,26 @@
-from flask import render_template, url_for
+from flask import render_template, redirect,url_for,abort,request
 from . import main
+from flask_login import login_required,current_user
+from ..models import User
+from ..import db, photos
+import secrets
+import os
+from ..email import mail_message
 
 @main.route('/')
 def index():
 
     return render_template('index.html')
+
+
+@main.route('/profile/<name>',methods = ['POST','GET'])
+@login_required
+def profile(name):
+    user = User.query.filter_by(username = name).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+
+    return render_template('profile/profile.html',user = user)
