04928ca488c83ee036582ca53a2e781e64afc33b
sling254 <mwanikidenis7@gmail.com>
2021-11-12 13:04:38 +0300

    add register and logout views

--- app/auth/views.py
+++ app/auth/views.py
@@ -20,3 +20,18 @@ def login():
   return render_template('auth/login.html', form = form)
 
 
+@auth.route('/register', methods = ["GET","POST"])
+def register():
+  form = RegForm()
+  if form.validate_on_submit():
+    user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+    user.save_u()
+    mail_message("Welcome to the Pitch","email/welcome_user",user.email,user=user)
+    return redirect(url_for('auth.login'))
+  return render_template('auth/register.html', form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for("main.login"))
