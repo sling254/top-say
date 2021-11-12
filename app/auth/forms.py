d73e21d860f6a824f2515f39928933d68704596d
sling254 <mwanikidenis7@gmail.com>
2021-11-12 12:56:26 +0300

    add a Register Form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,3 +8,19 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators=[Required()])
     remember_me = BooleanField('Remember Me!')
     submit = SubmitField('Login')
+
+
+class RegForm(FlaskForm):
+    email = StringField('Your Email Address', validators=[Required(),Email()])
+    username = StringField('Enter Your Username', validators=[Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError("The Email has already been taken!")
+    
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError("The username has already been taken")
