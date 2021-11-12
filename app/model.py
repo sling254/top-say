ea1d20c4879db1c5bd47883fda17e70f34393da6
sling254 <mwanikidenis7@gmail.com>
2021-11-12 12:44:08 +0300

    add Quote Class

--- app/model.py
+++ app/model.py
@@ -93,3 +93,10 @@ class Comment(db.Model):
   def __repr__(self):
     return f'Comment {self.comment}'
 
+class Quote:
+  """
+  Blueprint class for quotes consumed from API
+  """
+  def __init__(self, author, quote):
+    self.author = author
+    self.quote = quote
