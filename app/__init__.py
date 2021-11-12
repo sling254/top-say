bcf27822ad712fbc2035d605739a4235490517de
sling254 <mwanikidenis7@gmail.com>
2021-11-12 23:28:36 +0300

    add requirements.txt add image to register.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,23 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_login import  LoginManager
+from flask_mail import Mail
+
+
+
+#database 
+db = SQLAlchemy()
+#login Hellper classes
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+#Upload files
+photos = UploadSet('photos',IMAGES)
+
+#send mail
+mail = Mail()
 
 
 def create_app(config_name):
@@ -12,6 +30,16 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    
+    #Intializing flask extensions
+    db.init_app(app)#databse
+    login_manager.init_app(app)#login helper
+    mail.init_app(app)#mail
+
+    # configure UploadSet
+    configure_uploads(app,photos)
 
      # setting config
  
--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import ValidationError, StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
-from ..models import User
+from ..models import User 
 
 class LoginForm(FlaskForm):
     email = StringField('Your Email Address', validators=[Required(),Email()])
