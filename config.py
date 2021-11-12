af1bb59cace0ad2d7bea66874da021638f2aac4d
sling254 <mwanikidenis7@gmail.com>
2021-11-12 23:46:55 +0300

    add migrations file

--- config.py
+++ config.py
@@ -42,6 +42,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
     DEBUG = True
 
