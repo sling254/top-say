7942895821db012584560fffe94c94daf72fe141
sling254 <mwanikidenis7@gmail.com>
2021-11-12 13:11:25 +0300

    make configarations for the application

--- config.py
+++ config.py
@@ -1,8 +1,24 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 class Config:
     '''
     General configuration parent class
-    '''    
+    '''   
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+
+    DEBUG = True
+    #Route to photos
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+     
 
     DEBUG = True
     
--- manage.py
+++ manage.py
@@ -1,5 +1,9 @@
-from app import create_app
-from flask_script import Manager,Server
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app,db
+from app.models import User
+
+
 
 # Creating app instance
 app = create_app('development')
@@ -7,9 +11,13 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
 @manager.shell
 def make_shell_context():
-    return dict(app = app)
+    return dict(app = app,db=db, User=User)
     
 @manager.command
 def test():
