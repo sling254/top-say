4072f766a257694523a3ff3c5dab92b5671175a4
sling254 <mwanikidenis7@gmail.com>
2021-11-13 12:18:11 +0300

    add an updateProfile form and an Updateprofile view

--- app/auth/views.py
+++ app/auth/views.py
@@ -34,4 +34,4 @@ def register():
 @login_required
 def logout():
   logout_user()
-  return redirect(url_for("main.login"))
+  return redirect(url_for("main.index"))
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect,url_for,abort,request
 from . import main
 from flask_login import login_required,current_user
+from .forms import UpdateProfile
 from ..models import User
 from ..import db, photos
 import secrets
@@ -24,3 +25,20 @@ def profile(name):
         db.session.commit()
 
     return render_template('profile/profile.html',user = user)
+
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    user = User.query.filter_by(username = name).first()
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.bio = form.bio.data
+        db.session.commit()
+        return redirect(url_for('main.profile',name=user.username,))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.bio.data = current_user.bio
+    return render_template('profile/update.html', user = user, form =form)
