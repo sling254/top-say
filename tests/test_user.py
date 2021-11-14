13f1341147e7c707bff681bb556859d068cff36d
sling254 <mwanikidenis7@gmail.com>
2021-11-14 13:21:06 +0300

    add unittests and link the home page

---
+++ tests/test_user.py
@@ -0,0 +1,16 @@
+import unittest
+
+from app.models import User
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User(username='sling',password='123456')
+   
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('123456'))
+
